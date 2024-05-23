91bd84266f7d78c032e6ed24c49917148938def5
mendres82 <152789928+mendres82@users.noreply.github.com>
2024-05-23 03:11:15 +0200

    Fixes to Shell scripts

--- turbostat.sh
+++ turbostat.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-killall -s SIGKILL turbostat; turbostat --Summary --quiet --show PkgWatt --interval 5 --out /tmp/turbostat.tmp &
+killall -s SIGKILL turbostat;
+turbostat --Summary --quiet --show PkgWatt --interval 5 --out /tmp/turbostat.tmp &
 
