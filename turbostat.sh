e85a8313ee72ec5dce162622db862319f41808fc
mendres82 <152789928+mendres82@users.noreply.github.com>
2025-02-07 13:03:09 +0100

    Added workaround for wrong turbostat outputs

--- turbostat.sh
+++ turbostat.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-killall -s SIGKILL turbostat;
+
+if pgrep -f "turbostat.sh" | grep -v "$$" > /dev/null; then
+    exit 0
+fi
+
+pkill -f "turbostat --Summary --quiet --show PkgWatt"
 turbostat --Summary --quiet --show PkgWatt --interval 5 --out /tmp/turbostat.tmp &
+
+(
+while true; do
+    if [ "$(tail -1 /tmp/turbostat.tmp)" = "0.00" ]; then
+        pkill -f "turbostat --Summary --quiet --show PkgWatt"
+        turbostat --Summary --quiet --show PkgWatt --interval 5 --out /tmp/turbostat.tmp &
+    fi
+    sleep 5
+done
+) &
+
+exit 0
 
